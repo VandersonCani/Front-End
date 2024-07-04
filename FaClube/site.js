45d7c9164f76cf849a6883a0475b8bf3f7acb693
Vanderson Cani <vanderson.alex.cani@gmail.com>
2024-07-03 21:00:06 -0300

    Terminado Ot 9

--- FaClube/site.js
+++ FaClube/site.js
@@ -2,16 +2,16 @@
 function validaInscrevase() {
 
     var nome = document.frmInscrevase.txtnome.value;
-    var expRegNome = new RegExp("^[A-Z][a-zà-ü]* ([A-z]{1}[a-zà-ü]* ){1,}");
+    var expRegNome = new RegExp("^[A-Z][a-zà-ü]*\\s([A-z]{1}[a-zà-ü]*\\s?){1,}");
   
     if (!expRegNome.test(nome)) {
         alert("Digite o Nome Corretamente");
         document.frmInscrevase.txtnome.focus();
         return false;
     }
-   /*  var email = document.frmInscrevase.txtemail.value;
-    var expRegEmail = new RegExp("^[a-z]{2,}+[@]+[a-z]{1,}+[.]+[c][o][m]$");
-  
+    var email = document.frmInscrevase.txtemail.value;
+    var expRegEmail = new RegExp("^[a-z]{1,}(.?[a-z]{2,})*@[a-z]{2,}.[c][o][m]([.a-z])?$");
+
     if (!expRegEmail.test(email)) {
         alert("Digite o email corretamente");
         document.frmInscrevase.txtemail.focus();
@@ -24,6 +24,6 @@ function validaInscrevase() {
         alert("Digite a Senha corretamente");
         document.frmInscrevase.txtsenha.focus();
         return false;
-    } */
+    }
     return true;
 }
